7373ad8d907d6253db2c4924924eaa87ed8668b7
James McKee <jmckee@blackberry.com>
2014-07-14 13:49:32 -0400

    Measurement data structure updates:
    -Add MeasurementCollection
    -Add params KeyValuePair[] constructor overload to Measurement to avoid forcing an empty parameter dictionary
    -Add ToString implementation to Measurement and MeasurementCollection

--- TsdLib.Common/Measurement.cs
+++ TsdLib.Common/Measurement.cs
@@ -1,20 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Net.Sockets;
 
 namespace TsdLib
 {
+    [Flags]
+    public enum MeasurementResult
+    {
+        Pass = 0,
+        Fail_Low = 1,
+        Fail_High = 2,
+        Fail = Fail_Low | Fail_High
+    }
+
     public struct Measurement<T>
         where T : IComparable<T>
     {
-        public T MeasuredValue;
-        public string Units;
-        public T LowerLimit;
-        public T UpperLimit;
-        public Dictionary<string, string> Parameters;
+        public readonly T MeasuredValue;
+        public readonly string Units;
+        public readonly T LowerLimit;
+        public readonly T UpperLimit;
+        public readonly Dictionary<string, string> Parameters;
 
-        public MeasurementResult Result;
-        public DateTime Timestamp;
+        public readonly MeasurementResult Result;
+        public readonly DateTime Timestamp;
 
         public Measurement(T measuredValue, string units, T lowerLimit, T upperLimit, Dictionary<string, string> parameters)
         {
@@ -42,9 +53,7 @@ namespace TsdLib
             Units = units;
             LowerLimit = lowerLimit;
             UpperLimit = upperLimit;
-            Parameters = new Dictionary<string, string>();
-            foreach (KeyValuePair<string, string> keyValuePair in parameters)
-                Parameters.Add(keyValuePair.Key, keyValuePair.Value);
+            Parameters = parameters.ToDictionary(kvp => kvp.Key, kvp => kvp.Value);
 
             if (measuredValue.CompareTo(lowerLimit) < 0)
                 Result = MeasurementResult.Fail_Low;
@@ -53,15 +62,31 @@ namespace TsdLib
             else
                 Result = MeasurementResult.Pass;
         }
+
+        public override string ToString()
+        {
+            return ToString(",");
+        }
+
+        public string ToString(string separator)
+        {
+            string pToString = string.Join(separator, Parameters.Select(kvp => kvp.Key + "=" + kvp.Value));
+            return string.Join(separator, MeasuredValue, Units, LowerLimit, UpperLimit, Result, pToString).TrimEnd(',');
+        }
     }
 
-    [Flags]
-    public enum MeasurementResult
+    public class MeasurementCollection<T> : BindingList<Measurement<T>>
+        where T : IComparable<T>
     {
-        Pass = 0,
-        Fail_Low = 1,
-        Fail_High = 2,
-        Fail = Fail_Low | Fail_High
+        public override string ToString()
+        {
+            return ToString(Environment.NewLine, ",");
+        }
+
+        public string ToString(string rowSeparator, string columnSeparator)
+        {
+            return string.Join(rowSeparator, this.Select(meas => meas.ToString(columnSeparator)));
+        }
     }
 
 }
