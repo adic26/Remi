8f0255848747ae41c339fb6c6147eec72a22f212
James McKee <jmckee@blackberry.com>
2014-07-14 11:11:07 -0400

    Remove InstrumentWrapper base class dependency from AIM4170C_Wrapper

--- TsdLib.Instruments/InstrumentWrappers/AIM4170C_Wrapper.cs
+++ TsdLib.Instruments/InstrumentWrappers/AIM4170C_Wrapper.cs
@@ -1,14 +1,12 @@
 ﻿namespace TsdLib.Instrument
 {
-    public class AIM4170C_Wrapper : InstrumentWrapper<AIM4170C>
+    public class AIM4170C_Wrapper
     {
+        private AIM4170C _aim4170C;
+
         public AIM4170C_Wrapper(AIM4170C aim4170C)
-            : base(aim4170C)
         {
-            
+            _aim4170C = aim4170C;
         }
-
-
     }
-
 }
