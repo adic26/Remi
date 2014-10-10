4b415664d77e7bcf2a70f99fb7465fadcf1ca19d
James McKee <jmckee@blackberry.com>
2014-10-10 12:01:36 -0400

    Add missing files from TsdLib.TestResults Fix .gitIgnore to allow the TestResults namespace folder Minor updates to TsdLib.Instruments.xsd

---
+++ TsdLib/TestResults/MeasurementParameter.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TsdLib.TestResults
+{
+    /// <summary>
+    /// A test condition or other information used to describe test conditions.
+    /// </summary>
+    [Serializable]
+    public class MeasurementParameter
+    {
+        /// <summary>
+        /// Name of the parameter.
+        /// </summary>
+        public readonly string Name;
+        /// <summary>
+        /// Value of the parameter.
+        /// </summary>
+        public readonly object Value;
+
+        /// <summary>
+        /// Initialize a new MeasurementParameter object with the specified name and value.
+        /// </summary>
+        /// <param name="name">Name of the parameter.</param>
+        /// <param name="val">Value of the parameter.</param>
+        public MeasurementParameter(string name, object val)
+        {
+            Name = name;
+            Value = val;
+        }
+
+        /// <summary>
+        /// Returns a string representation of the MeasurementParameter.
+        /// </summary>
+        /// <returns>A string containing the name and value, separated by an equals sign.</returns>
+        public override string ToString()
+        {
+            return string.Concat(Name, "=", Value);
+        }
+    }
+
+    /// <summary>
+    /// Represents a collection of MeasurementParameter objects.
+    /// </summary>
+    [Serializable]
+    public class MeasurementParameterCollection : List<MeasurementParameter>
+    {
+        /// <summary>
+        /// Initialize an empty MeasurementParameterCollection.
+        /// </summary>
+        public MeasurementParameterCollection() { }
+
+        /// <summary>
+        /// Initialize a new MeasurementParameterCollection using a sequence of existing MeasurementParameter objects.
+        /// </summary>
+        /// <param name="parameters">A sequence of existing MeasurementParameter objects.</param>
+        public MeasurementParameterCollection(IEnumerable<MeasurementParameter> parameters)
+            : base(parameters) { }
+
+        /// <summary>
+        /// Gets the value of a MeasurementParameter by name.
+        /// </summary>
+        /// <param name="measurementName">Name of the MeasurementParameter.</param>
+        /// <returns>Value of the MeasurementParameter with the specified name.</returns>
+        public object GetMeasurementValue(string measurementName)
+        {
+            MeasurementParameter mp = this.FirstOrDefault(p => p.Name == measurementName);
+            if (mp == null)
+                throw new MeasurementParameterException(measurementName);
+            return mp.Value;
+        }
+
+        /// <summary>
+        /// Returns a string representation of the MeasurementParameters in the collection, with MeasurementParameters separated by a comma.
+        /// </summary>
+        /// <returns>A string representation of the MeasurementParameters in the collection.</returns>
+        public override string ToString()
+        {
+            return ToString(",");
+        }
+
+        /// <summary>
+        /// Returns a string representation of the MeasurementParameters in the collection, separated by the specified delimiter.
+        /// </summary>
+        /// <param name="separator">Delimiter string to insert between each MeasurementParameter object.</param>
+        /// <returns>A string representation of the MeasurementParameters in the collection.</returns>
+        public string ToString(string separator)
+        {
+            return string.Join(separator, this.Select(p => p.ToString()));
+        }
+    }
+    
+}
---
+++ TsdLib/TestResults/MeasurmentResult.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace TsdLib.TestResults
+{
+    /// <summary>
+    /// Describes possible measurement outcomes.
+    /// </summary>
+    [Flags]
+    public enum MeasurementResult
+    {
+        /// <summary>
+        /// The measurement has not yet been performed.
+        /// </summary>
+        Undefined = 0,
+        /// <summary>
+        /// The measurement is within the specified range.
+        /// </summary>
+        Pass = 1,
+        /// <summary>
+        /// The measurement is below the specified range.
+        /// </summary>
+        Fail_BelowLimit = 2,
+        /// <summary>
+        /// The measurement is above the specified range.
+        /// </summary>
+        Fail_AboveLimit = 4,
+        /// <summary>
+        /// The measurement is outside the specified range.
+        /// </summary>
+        Fail = Fail_BelowLimit | Fail_AboveLimit
+    }
+}
