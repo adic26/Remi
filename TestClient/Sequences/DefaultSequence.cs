f3f9001004b2eb8c02be49455a25e942d06c669d
Kevin Jin <kjin@blackberry.com>
2014-12-10 15:05:40 -0500

    Added a TsdLib compatible SSH client library using plink

--- TestClient/Sequences/DefaultSequence.cs
+++ TestClient/Sequences/DefaultSequence.cs
@@ -1,4 +1,5 @@
 
+using System.Diagnostics;
 using System.Threading;
 using TestClient.Configuration;
 using TestClient.Instruments;
@@ -14,10 +15,14 @@ namespace TestClient.Sequences
             //TODO: Create test sequence. This is the step-by-step sequence of instrument and/or DUT commands and measurements
 
             //Use the System.Diagnostics.Debugger.Break() method to insert breakpoints.
-            //Debugger.Break();
+            Debugger.Break();
 
+            
+            BlackBerryBattery bbb = BlackBerryBattery.Connect();
+            Measurements.Add(new Measurement<string>("IMEI", bbb.GetImei(), "imei", "asdf", "asdf"));
+
+            /*
             PowerSupply ps = new DummyPowerSupply("addr");
-
             for (int i = 0; i < testConfig.LoopIterations; i++)
             {
                 foreach (double voltageSetting in testConfig.VoltageSettings)
@@ -35,7 +40,7 @@ namespace TestClient.Sequences
                     Measurement<double> measurement = new Measurement<double>("Current", ps.GetCurrent(), "Amps", 0.1, 0.8, parameters: measurementParameters);
                     Measurements.Add(measurement);
                 }
-            }
+            }*/
         }
     }
 }
---
+++ TsdLib.Instrument.Ssh/SshFactory.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using RootDevice.Utilities;
+
+namespace TsdLib.Instrument.Ssh
+{
+    public class SshFactory : FactoryBase<SshConnection>
+    {
+        public int numRetries = 2;
+
+        private KeyValuePair<string, string> _rtasCredentials = new KeyValuePair<string, string>(
+            "r8_rel_lab",
+            "r8r3liability");
+        private string devicePass = null;
+
+        protected override IEnumerable<string> SearchForInstruments()
+        {
+            return ConnectionUtility.GetDeviceAddresses();
+        }
+        protected override SshConnection CreateConnection(string address, int defaultDelay, params ConnectionSettingAttribute[] attributes)
+        {
+            if (!ConnectionUtility.GetDeviceResponse(address).Contains("BlackBerry Device"))
+                return null;
+
+            var exceptions = new List<Exception>();
+            for (int i = numRetries; i > 0; i--)
+            {
+                try
+                {
+                    return new SshConnection(_rtasCredentials.Key, _rtasCredentials.Value, address, devicePass);
+                }
+                catch(Exception e)
+                {
+                    exceptions.Add(e);
+                }
+            }
+            return null;
+        }
+        protected override string GetInstrumentIdentifier(SshConnection connection, IdQueryAttribute idAttribute)
+        {
+            string identifier =
+                (from ni in NetworkInterface.GetAllNetworkInterfaces()
+                 from ip in ni.GetIPProperties().DhcpServerAddresses
+                 where ip.ToString() == connection.Address
+                 select ni.Description)
+                .FirstOrDefault();
+
+            if (identifier == null)
+                throw new Exception("Could not locate instrument with address " + connection.Address);
+
+            return identifier;
+        }
+    }
+}
