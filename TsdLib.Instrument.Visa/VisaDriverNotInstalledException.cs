5b8d3384075fd3121337a4290bd1ba6dc6cb4da3
James McKee <jmckee@blackberry.com>
2015-01-27 14:31:48 -0500

    Remove auto-installation of Visa drivers from application - move it to installer

--- TsdLib.Instrument.Visa/VisaDriverNotInstalledException.cs
+++ TsdLib.Instrument.Visa/VisaDriverNotInstalledException.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Diagnostics;
 
 namespace TsdLib.Instrument.Visa
 {
@@ -8,14 +7,7 @@ namespace TsdLib.Instrument.Visa
         public VisaDriverNotInstalledException(Exception inner = null)
             : base("The National Instruments VISA driver or required assemblies are missing from this PC. Please install the drivers and make sure to select .NET Framework 4.5 Language Support in the installer.", inner)
         {
-            try
-            {
-                Process.Start("NIVISA1401full_downloader.exe");
-            }
-            catch (Exception ex)
-            {
-                Trace.WriteLine(ex);
-            }
+
         }
     }
 }
