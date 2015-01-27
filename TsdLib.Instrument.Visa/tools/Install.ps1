a3f98222a6c8fecf55406df0ac49551503255a64
James McKee <jmckee@blackberry.com>
2015-01-27 10:25:00 -0500

    Add Visa downloader to Instrument.Visa nuget package

---
+++ TsdLib.Instrument.Visa/tools/Install.ps1
@@ -0,0 +1,33 @@
+﻿param($installPath, $toolsPath, $package, $project)
+
+$regKey = 'HKLM:Software\National Instruments\NI-VISA\CurrentVersion'
+
+If (!(Test-Path($regKey)))
+{
+    Write-Host "No Registry key for NI-VISA drivers"
+    Install-Drivers
+}
+Else
+{
+    $val = (Get-ItemProperty -Path 'HKLM:Software\National Instruments\NI-VISA\CurrentVersion' -Name "Version").Version
+    If ($val -ne "14.0.11")
+    {
+        Write-Host "Wrong version for NI-VISA drivers"
+        Install-Drivers
+    }
+}
+
+function Install-Drivers
+{
+    Write-Host "Running VISA driver downloader"
+    Start-Process -FilePath $toolsPath + "\NIVISA1401full_downloader.exe"
+}
+
+function Install-Drivers-From_msi
+{
+    Write-Host "Installing VISA drivers"
+    $installSwitch = "/i"
+    $msiPath = $toolsPath + "\NIVISA1401full_downloader.exe"
+    $features = "ADDLOCAL='.NET Framework 4.5 Language Support"
+    Start-Process -FilePath $msiPath -ArgumentList /i, $msiPath
+}
