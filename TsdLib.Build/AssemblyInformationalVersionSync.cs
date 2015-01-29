969a6865988e07d8a2e1158b033eb8d7a1a992c6
James McKee <jmckee@blackberry.com>
2015-01-29 11:58:06 -0500

    in progress

--- TsdLib.Build/AssemblyInformationalVersionSync.cs
+++ TsdLib.Build/AssemblyInformationalVersionSync.cs
@@ -1,13 +1,28 @@
 ﻿
+using System.Reflection;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Tasks;
 using Microsoft.Build.Utilities;
 
 namespace TsdLib.Build
 {
-    public class AssemblyInformationalVersionSync : Task
+    public class AssemblyInformationalVersionSync : GetAssemblyIdentity
     {
+        [Output]
+        public string AssemblyInformationalVersionAttributeValue { get; set; }
+
+        /// <summary>
+        /// Executes the task to create an <see cref="AssemblyInformationalVersionAttribute"/> with a value equal to the <see cref="AssemblyVersionAttribute"/>.
+        /// </summary>
+        /// <returns>True if the task successfully executed; otherwise, false.</returns>
         public override bool Execute()
         {
-            throw new System.NotImplementedException();
+            base.Execute();
+
+            var str = Assemblies;
+
+
+            return true;
         }
     }
 }
