f54474549b62e1ba1d44d69740371bb4346dd066
James McKee <jmckee@blackberry.com>
2015-01-30 13:40:31 -0500

    in progress

--- GitVersionTest/Properties/AssemblyInfo.cs
+++ GitVersionTest/Properties/AssemblyInfo.cs
@@ -1,15 +1,15 @@
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("BuildToolsTest2")]
-[assembly: AssemblyDescription("Test project for custom MSBuild tasks")]
+[assembly: AssemblyTitle("GitVersionTest")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("BlackBerry")]
-[assembly: AssemblyProduct("BuildToolsTest2")]
+[assembly: AssemblyProduct("GitVersionTest")]
 [assembly: AssemblyCopyright("Copyright © BlackBerry 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("9bd6008e-0362-4b78-86d7-ff1a85ca03e6")]
+[assembly: Guid("6ed5e715-6d1b-45a3-b2b4-f625a1997593")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,4 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
