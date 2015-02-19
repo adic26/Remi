190f670fef5823b96784464c9c548f896c800bde
James McKee <jmckee@blackberry.com>
2015-02-19 11:10:45 -0500

    Add missing files

---
+++ TsdLib.Forms/PropertyGridEditor.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows.Forms;
+
+namespace TsdLib.Forms
+{
+    public partial class PropertyGridEditor : Form
+    {
+        /// <summary>
+        /// Initialize a new <see cref="PropertyGridEditor"/>
+        /// </summary>
+        /// <param name="obj">An object with public properties.</param>
+        public PropertyGridEditor(object obj)
+        {
+            InitializeComponent();
+            
+            propertyGrid.SelectedObject = obj;
+
+            Text = obj.GetType().Name + " Editor";
+        }
+
+        public override sealed string Text
+        {
+            get { return base.Text; }
+            set { base.Text = value; }
+        }
+    }
+}
