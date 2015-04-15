749d022bc20246192c4d295ff38e05e2d79981f3
James McKee <jmckee@blackberry.com>
2015-04-15 15:59:13 -0400

    mend

---
+++ TsdLib.Instrument.Adb/AdbConnection_Process_StartStop.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text;
+
+namespace TsdLib.Instrument.Adb
+{
+    /// <summary>
+    /// Contains functionality to communicate with an instrument using adb.exe.
+    /// </summary>
+    public class AdbConnection_Process_StartStop : ConnectionBase
+    {
+        private readonly string _adbExe;
+        private readonly StringBuilder _output;
+        private readonly StringBuilder _error;
+        private int _exitCode;
+
+        internal AdbConnection_Process_StartStop(string address, string adbFileLocation)
+            : base(address)
+        {
+            _output = new StringBuilder();
+            _error = new StringBuilder();
+            _adbExe = adbFileLocation;
+        }
+
+        protected override void Write(string message)
+        {
+            _output.Clear();
+            _error.Clear();
+            string arguments = message.StartsWith("adb") ? message : string.Format(@"/c {0} -s {1} shell ""{2}""", _adbExe, Address, message);
+            ProcessStartInfo startInfo = new ProcessStartInfo
+            {
+                FileName = "cmd.exe",
+                Arguments = arguments,
+                CreateNoWindow = true,
+                UseShellExecute = false,
+                RedirectStandardError = true,
+                RedirectStandardOutput = true
+            };
+
+            Process process = Process.Start(startInfo);
+            if (process== null || !process.WaitForExit(5000))
+                throw new Exception(string.Format("Command failed: {0}. Timeout waiting for Adb process to exit", message));
+
+            _output.Append(process.StandardOutput.ReadToEnd().Trim());
+            _error.Append(process.StandardError.ReadToEnd().Trim());
+
+            _exitCode = process.ExitCode;
+        }
+
+        protected override string ReadString()
+        {
+            string response = _output.ToString();
+            _output.Clear();
+            return response;
+        }
+
+        protected override byte ReadByte()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override bool CheckForError()
+        {
+            Trace.WriteLineIf(_exitCode != 0, "ADB ERROR: Process exit code = " + _exitCode);
+            Trace.WriteLineIf(_error.Length != 0, "ADB ERROR: " + _error);
+            return _exitCode != 0 || _error.Length != 0;
+        }
+
+        public override bool IsConnected
+        {
+            get { return true; }
+        }
+    }
+}
---
+++ TsdLib.Instrument.Adb/AdbFactory_Process_Start_Stop.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+
+namespace TsdLib.Instrument.Adb
+{
+    /// <summary>
+    /// Contains functionality to discover and connect to instruments via adb.exe.
+    /// </summary>
+    public class AdbFactory_Process_Start_Stop : FactoryBase<AdbConnection_Process_StartStop>
+    {
+        private const string AdbFileLocation = @"platform-tools\adb.exe";
+
+        protected override IEnumerable<string> SearchForInstruments()
+        {
+            ProcessStartInfo startInfo = new ProcessStartInfo
+            {
+                UseShellExecute = false,
+                CreateNoWindow = true,
+                FileName = AdbFileLocation,
+                Arguments = "devices",
+                RedirectStandardError = true,
+                RedirectStandardOutput = true
+            };
+
+            List<string> addresses = new List<string>();
+
+            using (Process queryDevicesProcess = new Process { StartInfo =  startInfo})
+            {
+                queryDevicesProcess.ErrorDataReceived += process_ErrorDataReceived;
+                queryDevicesProcess.Start();
+
+                string line;
+                while ((line = queryDevicesProcess.StandardOutput.ReadLine()) != null)
+                {
+                    Match match = Regex.Match(line, @"\w{8}(?=\sdevice)");
+                    if (match.Success)
+                        addresses.Add(match.Value);
+                }
+                queryDevicesProcess.WaitForExit(2000);
+                if (!queryDevicesProcess.HasExited)
+                    throw new AdbConnectException("Adb device query process did not terminate");
+            }
+            
+            return addresses;
+        }
+
+        void process_ErrorDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            throw new AdbConnectException("Error received when searching for devices: " + e.Data);
+        }
+
+        protected override AdbConnection_Process_StartStop CreateConnection(string address, int defaultDelay, params ConnectionSettingAttribute[] attributes)
+        {
+            ProcessStartInfo startInfo = new ProcessStartInfo
+            {
+                UseShellExecute = false,
+                CreateNoWindow = true,
+                FileName = AdbFileLocation,
+                Arguments = "root",
+                RedirectStandardError = true,
+                RedirectStandardOutput = true
+            };
+
+            using (Process rootProcess = Process.Start(startInfo))
+            {
+                if (rootProcess == null)
+                    throw new AdbConnectException("Could not start adb.exe to send the root command");
+                rootProcess.WaitForExit(10000);
+                if (!rootProcess.HasExited)
+                    throw new AdbConnectException("Root process did not terminate");
+            }
+            AdbConnection_Process_StartStop conn = new AdbConnection_Process_StartStop(address, AdbFileLocation);
+
+            return conn;
+        }
+
+        protected override string GetInstrumentIdentifier(AdbConnection_Process_StartStop connection, IdQueryAttribute idAttribute)
+        {
+            return "Avengers";
+        }
+    }
+
+
+}
